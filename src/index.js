ed3280fa6f09b90b006431dcfcb2b2252bc59d8a
github-mariyaweb <96mariyask@gmail.com>
2023-01-24 12:55:46 +0300

    feat: add toReadable function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+
+    const prefixTens = ["", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    let numberString = number.toString();
+
+    if (number == 0) {
+        return "zero";
+    }
+
+    if (number < 20) {
+        return ones[number];
+    }
+
+    if (numberString.length == 2) {
+        return prefixTens[numberString[0]] + " " + ones[numberString[1]];
+    }
+
+    if (numberString.length == 3) {
+        if (numberString[1] === "0" && numberString[2] === "0") {
+            return ones[numberString[0]] + " hundred";
+        } else {
+            return ones[numberString[0]] + " hundred " + toReadable(+(numberString[1] + numberString[2]));
+        }
+    }
+
 }
+console.log(toReadable(999));
+
